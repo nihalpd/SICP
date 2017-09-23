f96773b6cdb7081f66ae40711b35a85b961e9b41
Nihal Dayal <nihaldayal@gmail.com>
2017-09-23 13:07:18 -0700

    Correct 1.6

--- ch1/ex6.scm
+++ ch1/ex6.scm
@@ -1,6 +1,7 @@
 ; ex6.scm
+; Chapter 1
+; Exercise 6
 
-
-(define (average x y)
-  (/ (+ x y) 2)) 
-
+; new-if is a function.  this means that all of its arguments will be
+; evaluated before the function itself is evaluated. this will cause 
+; srt-iter to become caught in an infinite recursion.
